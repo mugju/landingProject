e9580d785e75d6ffb40a0d49a379089a9001e248
DongD <ddaa1155k@gmail.com>
2022-04-12 16:41:03 +0900

    로그인/로그아웃 관련 브랜치 생성 urls.py 생성

--- Server/config/urls.py
+++ Server/config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('employee/', include('employee.urls')),
+    path('user/', include('user.urls'))
 ]
