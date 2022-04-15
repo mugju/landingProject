0d9886d0fa940768d4980369f319edcab977cdaf
DongD <ddaa1155k@gmail.com>
2022-04-15 11:15:29 +0900

    비밀번호 재설정, 회원정보 재설정 함수 구현

--- Server/user/urls.py
+++ Server/user/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
     path('signin/',views.signin),
     path('signup/', views.signup),
     path('find/',views.pw_find),
+    path('set/',views.pw_set),
+    path('<int:user_uid>/', views.edit_user),
 
+
+
+    path('signout/',views.signout)
 ]
--- Server/user/views.py
+++ Server/user/views.py
@@ -1,85 +1,138 @@
 from django.shortcuts import render
 from django.contrib import auth
 from django.contrib.auth import authenticate
+from django.contrib.auth import update_session_auth_hash
 import json
 from .models import User
 
 from django.http import HttpResponse
 
+
 # ==============로그인 함수=================
 def signin(request):
     if request.method == "POST":
         user_data = json.loads(request.body)  # JSON data parsing / 여기에선 로그인 정보.
-        user_email  = user_data["user_email"]
+        user_email = user_data["user_email"]
         user_password = user_data["user_pw"]
 
-        user = authenticate(request, password=user_password, username=user_email) # 유저 인증과정
+        user = authenticate(request, password=user_password, username=user_email)  # 유저 인증과정
 
-        request.session['user_uid'] = user.user_uid # 세션을 통해 uid 넘겨줌
-
-        if user is None:    # 회원정보 없는 경우.
+        if user is None:  # 회원정보 없는 경우.
             result = "회원정보 없음."
+            print(user)
         else:
             auth.login(request, user)
             result = "ok"
-        output ={
-            "session_uid" : request.session.get('user_uid'),
-            "message" : result
+            request.session['user_uid'] = user.user_uid  # 세션을 통해 uid 넘겨줌
+        output = {
+            "user_uid": request.session.get('user_uid'),
+            "message": result
         }
     return HttpResponse(json.dumps(output),
                         content_type=u"application/json; charset=utf-8",
                         status=200)
 
+
 # ===============회원가입 함수===============
 def signup(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         print("회원가입 로직")
         user_data = json.loads(request.body)  # JSON data parsing / 여기에선 회원가입 정보.
 
-        if user_data["user_pw"] == user_data["user_pw_confirm"]:     # 비밀번호 확인
+        if user_data["user_pw"] == user_data["user_pw_confirm"]:  # 비밀번호 확인
             user = User.objects.create_user(
-                user_email = user_data["user_email"],
-                password = user_data["user_pw"],
-                user_storename = user_data["user_storename"]
+                user_email=user_data["user_email"],
+                password=user_data["user_pw"],
+                user_storename=user_data["user_storename"]
             )
             auth.login(request, user)
             output = {"message": "장고 기본 테이블에 회원가입 진행."}
 
         else:
             output = {"message": "비밀번호 확인 실패."}
-        
+
     else:  # post 이외 방식
-        output = {"message" : "잘못된 접근."}
-    
+        output = {"message": "잘못된 접근."}
+
     return HttpResponse(json.dumps(output),
                         content_type=u"application/json; charset=utf-8",
                         status=200)
 
+
 # 패스워드 찾기 함수 ==> 회원정보를 찾아 json으로 ok 응답 넘김.
 def pw_find(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         user_data = json.loads(request.body)
 
-        try: # 입력정보 기반 db에서 회원정보 탐색.
+        try:  # 입력정보 기반 db에서 회원정보 탐색.
             user = User.objects.get(user_email=user_data["user_email"])
             print(user.user_storename)
             if user.user_storename == user_data["user_storename"]:
-                output = {"message": "회원정보 일치, 비밀번호 리셋으로 리다이렉트"}
+                request.session['user_uid'] = user.user_uid
+                output = {
+                    "message": "회원정보 일치, 비밀번호 리셋으로 리다이렉트",
+                    "user_uid": request.session["user_uid"]
+                }
+
             else:
                 output = {"message": "회원정보 불일치, 얼럿메시지 발행"}
-        except :
+        except:
             output = {"message": "해당 회원정보 없음.!!"}
 
-
-        return HttpResponse(json.dumps(output),
+        return HttpResponse(json.dumps(output, ensure_ascii=False),
                             content_type=u"application/json; charset=utf-8",
                             status=200)
 
-def pw_set(request):
-    return "ok"
 
-def edit_user(request):
-    return "ok"
+def pw_set(request):
+    if request.method == 'POST':
+        try:
+            user_uid = request.session["user_uid"]
+            print(user_uid)
+            output = {"message": "회원정보 있음."}
+            user = User.objects.get(user_uid=user_uid)
+            new_pw = json.loads(request.body)["user_new_pw"]
+            print("new pw : {}".format(new_pw))
+            user.set_password(new_pw)
+            user.save()
+
+
+        except:
+            output = {"message": "회원정보 없음, 얼럿메시지 발행"}
+
+    return HttpResponse(json.dumps(output, ensure_ascii=False),
+                        content_type=u"application/json; charset=utf-8",
+                        status=200)
+
+
+def edit_user(request,user_uid):
+    if request.method == 'PATCH':
+        try:
+            user_uid_s = request.session["user_uid"]
+            if user_uid == user_uid_s: # 세션 유저와 url상 유저가 동일.
+                user = User.objects.get(user_uid=user_uid)
+                user_data = json.loads(request.body)
+                user.user_email = user_data["user_email"]
+                user.user_storename = user_data["user_storename"]
+                user.save()
+                output = {"message": "유저정보 수정"}
+            else:
+                output = {"message": "세션 유저와 수정대상 유저가 동일하지 않습니다."}
+        except:
+            output = {"message": "세션내 유저정보가 없습니다."}
+
+        return HttpResponse(json.dumps(output, ensure_ascii=False),
+                            content_type=u"application/json; charset=utf-8",
+                            status=200)
+
 
 def delete_user(request):
     return "ok"
+
+
+def signout(request):
+    auth.logout(request)
+    output = {"message": "로그아웃"}
+    return HttpResponse(json.dumps(output, ensure_ascii=False),
+                        content_type=u"application/json; charset=utf-8",
+                        status=200)
