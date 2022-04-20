eaf736155bc9fe0bb01dbe8fc7a057200a9cdbfe
junu0810 <junu0810@hanmail.net>
2022-04-20 10:47:22 +0900

    fix git conplict

--- Server/bank/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Bank(models.Model):
-    bank_uid = models.AutoField(primary_key=True)
-    bank_name = models.CharField(max_length=15)
--- Server/request/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from bill.views import makeBill
-
-urlpatterns = [
-    path('bill', makeBill),
-    path('req', views.postReq),
-    path('req/<int:uid>', views.fixReq)
-]
