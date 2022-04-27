c22672e17d9ca188ad99033778a77c48c6ceec46
DongD <ddaa1155k@gmail.com>
2022-04-27 09:12:49 +0900

    로그기능 완료
    로그인/로그아웃/로그인실패 모든 경우에 대해
    user 이하 logtbl 모델에 작성됩니다.

--- Server/user/admin.py
+++ Server/user/admin.py
@@ -5,3 +5,11 @@ from . import models
 @admin.register(models.User)
 class UserAdmin(admin.ModelAdmin):
     pass
+
+from django.contrib import admin
+from .models import Logtbl
+
+@admin.register(Logtbl)
+class LogtblAdmin(admin.ModelAdmin):
+    list_display = ['action', 'username', 'ip','timestamp',]
+    list_filter = ['action','username']
