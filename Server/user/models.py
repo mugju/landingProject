eab4db7d0caefb8363c29474e03cc37ea6b7687b
DongD <ddaa1155k@gmail.com>
2022-04-27 09:12:49 +0900

    로그기능 완료
    로그인/로그아웃/로그인실패 모든 경우에 대해
    user 이하 logtbl 모델에 작성됩니다.

--- Server/user/models.py
+++ Server/user/models.py
@@ -68,11 +68,38 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 
 
+#  로그기능 추가.
+from django.db import models
+from django.contrib.auth.signals import user_logged_in, user_logged_out, user_login_failed
+from django.dispatch import receiver
 
-# class User(models.Model):
-#     user_uid = models.AutoField(primary_key=True)
-#     user_email = models.EmailField()
-#     user_pw = models.CharField(max_length=100)
-#     user_joindate = models.DateTimeField(auto_now=True)
-#     user_storename = models.CharField(max_length=20)
-#     user_session = models.CharField(max_length=30)
+
+class Logtbl(models.Model):
+    action = models.CharField(max_length=64)
+    ip = models.GenericIPAddressField(null=True)
+    username = models.CharField(max_length=256, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '{0} - {1} - {2}'.format(self.action, self.username, self.ip)
+
+    def __str__(self):
+        return '{0} - {1} - {2}'.format(self.action, self.username, self.ip)
+
+
+@receiver(user_logged_in)
+def user_logged_in_callback(sender, request, user, **kwargs):
+    ip = request.META.get('REMOTE_ADDR')
+    Logtbl.objects.create(action='user_logged_in', ip=ip, username=user.user_email)
+
+
+@receiver(user_logged_out)
+def user_logged_out_callback(sender, request, user, **kwargs):
+    ip = request.META.get('REMOTE_ADDR')
+    Logtbl.objects.create(action='user_logged_out', ip=ip, username=user.user_email)
+
+
+@receiver(user_login_failed)
+def user_login_failed_callback(sender, request, credentials, **kwargs):
+    ip = request.META.get('REMOTE_ADDR')
+    Logtbl.objects.create(action='user_login_failed',ip=ip, username=credentials.get('username', None))
