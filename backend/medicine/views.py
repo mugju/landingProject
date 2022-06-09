408e19fb6fe46b88307df18080265e1e4df8c1c7
mond1219 <lssgt07@naver.com>
2022-06-09 14:04:49 +0900

    medicine view.py comment add

--- backend/medicine/views.py
+++ backend/medicine/views.py
@@ -14,9 +14,19 @@ from datetime import datetime
 
 #세션에 담긴 uid가 User에 존재하는지 확인
 def medSession(request):
-    """
-        세션 uid 확인함수
+    """세션 uid 확인함수
+        Args:
+            request: 클라이언트로 부터의 요청
 
+        Returns:
+            output: 클라이언트의 user_uid를 반환함
+
+        Raises:
+            403{"message":'session ID not found'} : session에 ID가 존재하지 않은 경우
+            404{'message': 'user not found'} : 존재하지 않는 유저인 경우
+
+        Note:
+            med_index, med_detail 함수 도입부에 현재 함수를 통해 유저의 session id를 확인한다.
     """
     try:
         sessionId = request.session['auth'] #세션에 아이디가 있는지 없는지 확인
@@ -29,7 +39,23 @@ def medSession(request):
        return JsonResponse({'message': 'user not found'}, status =404)
 
 def med_index(request):
-    user_uid = medSession(request) #권한 없음 404 에러 발생
+    """GET, POST 방식의 medicine 페이지 접근함수
+        Allowed Method:
+            GET, POST
+        Args:
+            request: 클라이언트로 부터의 요청
+
+        Returns:
+            output(json):
+                GET 방식: 약품수, 약품 list, 회사 list
+                POST 방식: medicine 수정 여부
+
+        Raises:
+            400{'message':'bad input data'}: medicine의 형식에 맞지 않는 데이터를 넣었을 때
+            405{'message': 'method not allowed'}: GET,POST 가 아닌 방식으로 함수에 접근했을 때때
+
+    """
+    uer_uid = medSession(request)
     if type(user_uid) == JsonResponse:# session 확인 함수에서 error 발생시
         return user_uid
     else:
@@ -88,8 +114,19 @@ def med_index(request):
         else:# GET,POST가 아닐때
             return JsonResponse({'message': 'method not allowed'}, status =405)
 
-#medicine insert 함수
 def med_insert(request, user_uid):
+    """medicine 추가 함수
+        Args:
+            request: 클라이언트의 요청
+            user_uid: session에 있는 유저의 ID
+
+        Returns:
+            output : medicine 추가 성공 여부
+
+        Raises:
+            400 : 추가 실패 error 코드
+
+    """
     med = json.loads(request.body) #JSON data parsing
     try:
         medAdd = Medicine(user_uid=User.objects.get(user_uid = request.session['auth']),
@@ -119,6 +156,27 @@ def med_insert(request, user_uid):
             return "ok"
 
 def med_detail(request, med_uid):
+"""PATCH, DELETE 방식의 medicine 페이지 접근함수
+    Allowed Method: PATCH, DELETE
+
+    Args:
+        request : 클라이언트의 요청
+        med_uid : 약품 유효번호
+
+    Returns:
+        output(json) :
+            PATCH 방식: medicine 수정 성공 여부
+            DELETE 방식: delete 성공 여부
+
+    Raises:
+        400{'message':'bad input data'}: 수정할 데이터의 값이 유효하지 않을때
+        401{'message':'unauthorized'}: 수정, 삭제 요청자가 권한이 없을때
+        405{'message': 'method not allowed'}: PATCH, DELETE 가 아닌 다른 method 로 요청 했을때
+
+    Note:
+        여기서 발생하는 400,401error는 try문으로 발생하는 error가 아니라 editMedicine을 통해 발생한
+        error값이 return 되어 발생하는 것이다.
+"""
     #사용자 아이디 확인
     user_uid = medSession(request)
     if type(user_uid) == JsonResponse:# session 확인 함수에서 error 발생시
@@ -152,6 +210,22 @@ def med_detail(request, med_uid):
 
 #med_detail edit 함수
 def editMedicine(request, med_uid, user_uid):
+    """medicine 수정 함수
+        Args:
+            request: 클라이언트 요청
+            med_uid: 수정하는 medicine 유효번호
+            user_uid: 수정하려는 클라이언트이 user_uid
+
+        Returns:
+            nomal return: medicine 수정 여부 알려주는 메세지
+
+        Raises:
+            401: 존재하지 않는 medicine의 값을 변경하려고 했을 때, session의 user_uid와 medicine을 등록한 user_uid가 다를때
+            400: medicine 수정 데이터가 유효하지 않을때
+
+        Notes:
+            이 함수에서 error가 발생하면 except로 error코드를 return 하여 med_detail함수에서 error 처리를 해준다.
+    """
     med_edit = json.loads(request.body) #JSON data parsing
     try:
         medicine = Medicine.objects.get(med_uid=med_uid)
@@ -195,6 +269,18 @@ def editMedicine(request, med_uid, user_uid):
 
 #salt detail insert&update 함수
 def saltSave(salt_arr, med):
+    """medicine의 salt목록 insert, update 함수
+        Args:
+            salt_arr: insert및 update할 medicine의 salt list
+            med: insert및 update할 medicine의 Model, med_uid를 위해 필요하다.
+
+        Returns:
+            output: salt insert, update 성공 여부
+
+        Raises:
+            400: salt insert, update 실패할 시,(salt에 데이터에 유효하지 않은 데이터가 있을경우 발생생) 400 error return한다.
+
+    """
     try:
         for salt in salt_arr:
             med_salt = Med_salt(
